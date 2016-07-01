3a8ba433f23eb978d0f194f4fc4de38658b3345f
Dominic Tarr <dominic.tarr@gmail.com>
2016-07-01 17:48:13 +1200

    put a content element inside lightbox

--- index.js
+++ index.js
@@ -11,13 +11,15 @@ function width (el) {
 function px(p) { return p+'px' }
 
 module.exports = function () {
+  var content = h('div.lightbox__content')
   var lightbox = h('div.lightbox', {style: {
     position: 'fixed',
     top: px(0), bottom: px(0),
     left: px(0), right: px(0),
     display: 'none',
 //    padding: px(10)
-  }})
+  }}, content)
+
 
   lightbox.close = function () {
     lightbox.style.display = 'none'
@@ -29,12 +31,13 @@ module.exports = function () {
   }
 
   lightbox.show = function (el) {
-    lightbox.appendChild(el)
+    content.appendChild(el)
     lightbox.style.display = 'block'
     lightbox.center()
   }
 
   return lightbox
 }
+
 
 
